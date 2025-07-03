85cc1a95f9294b4e703be7d3e4052b0e38dc977e
cletus ola <cletushunsu@gmail.com>
2025-07-03 16:23:52 +0100

    Added logging

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify 
 from app import db 
 from app.models import User
+from flask import current_app as app
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 auth_bp = Blueprint('auth', __name__)
@@ -13,6 +14,9 @@ def register():
     email = data.get("email")
     password = data.get("password")
 
+    if not username or not email or not password:
+        return jsonify({"message": "Username, email, and password are required"})
+    
     if User.query.filter((User.username == username) | (User.email == email)).first():
         return jsonify({
             "message": "User with that username or email already exists"
@@ -23,6 +27,7 @@ def register():
     db.session.add(user)
     db.session.commit()
 
+    app.logger.info(f"New user registration: {email}")
     return jsonify({"message": "User registered successfully"}), 201
 
 
@@ -46,10 +51,17 @@ def login():
 @jwt_required
 def me():
     user_id = get_jwt_identity()
-    user = User.query.get(user_id)
-    return jsonify({
-        "id": user.id,
-        "username": user.username,
-        "email": user.email,
-        "created_at": user.created_at.isoformat()
-    }), 200 
+    if not user_id:
+        app.logger.warning(f"Unauthorized access attempt by user")
+    try:    
+        user = User.query.get(user_id)
+        return jsonify({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "created_at": user.created_at.isoformat()
+        }), 200 
+    
+    except Exception as e:
+        app.logger.exception("An unexpected error occurred")
+        return jsonify({"message": "Internal server error"}), 500
