e07d364e6c5098edcac3514507d017d493c9c2a1
cletus ola <cletushunsu@gmail.com>
2025-07-03 10:13:23 +0100

    Created user and post model

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timezone
+from app import db, bcrypt
+
+# user model 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(250, nullable=False))
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+    
+# post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
